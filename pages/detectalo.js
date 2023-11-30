e054c422c28977c06ea61e8200ef98f2c72e2597
bryanchopin <bryan.oliveros17@tectijuana.edu.mx>
2023-11-29 19:11:50 -0800

    Add ripple background effect to Detectalo page

--- pages/detectalo.js
+++ pages/detectalo.js
@@ -2,14 +2,20 @@ import React from "react";
 import Navbar from "@/components/navbar/navbar";
 import Footer from "@/layout/FooterContainer/footerContainer";
 import Preloader from "@/components/preloader/preloader";
+import styles from "@/styles/Home.module.css";
 
 export default function Detectalo() {
   return (
     <>
       <Preloader />
       <Navbar />
-      <h1>DETECTALO</h1>
-      <Footer />
+      <section className={styles['ripple-background']}>
+      <div className={`${styles.circle} ${styles.xxlarge} ${styles.shade1}`}></div>
+      <div className={`${styles.circle} ${styles.xlarge} ${styles.shade2}`}></div>
+      <div className={`${styles.circle} ${styles.large} ${styles.shade3}`}></div>
+      <div className={`${styles.circle} ${styles.medium} ${styles.shade4}`}></div>
+      <div className={`${styles.circle} ${styles.small} ${styles.shade5}`}></div>
+      </section>      <Footer />
     </>
   );
 }
